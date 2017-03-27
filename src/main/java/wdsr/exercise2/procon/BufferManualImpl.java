4c371bb4d4f004b9a8bbda75bc078d54868473a5
Blaszczyk <marcin.blaszczyk@accenture.com>
2017-03-27 09:08:59 +0200

    procon

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -1,16 +1,47 @@
 package wdsr.exercise2.procon;
 
+import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
  * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
 public class BufferManualImpl implements Buffer {
+	
+	private final Lock lock = new ReentrantLock();
+	private final Condition notFull = lock.newCondition();
+	private final Condition notEmpty = lock.newCondition();
+	private ArrayList<Order> orders = new ArrayList<Order>(100000);
+	
 	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+		lock.lock();
+		try{
+			while (orders.size() == 100000) {
+				notFull.await();
+			}
+			orders.add(order);
+			notEmpty.signal();
+			} finally {
+			lock.unlock();
+			}
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
 		// TODO
-		return null;
+		Order consumed = null;
+		lock.lock();
+		try{
+			while(orders.isEmpty()){
+				notEmpty.await();
+			}
+			consumed = orders.remove(0);
+			notFull.signal();
+			return consumed;
+		}finally{
+			lock.unlock();
+		}
 	}
 }
